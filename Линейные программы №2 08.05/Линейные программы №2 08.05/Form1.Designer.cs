a11496f9d70a782b452945af194a07d6348dd8f8
EvgenyVodyannikov <vodyannikov.eugene@gmail.com>
2020-06-08 15:20:10 +0500

    Пол работы сделано

--- Линейные программы №2 08.05/Линейные программы №2 08.05/Form1.Designer.cs
+++ Линейные программы №2 08.05/Линейные программы №2 08.05/Form1.Designer.cs
@@ -28,49 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Линейные_программы__2_08._05.Properties.Resources.q;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(392, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 66);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(469, 300);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Такси";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 83);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Пассажир 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(175, 267);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(284, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 259);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(163, 35);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Начать поездку";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 33);
+            this.textBox1.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(15, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(793, 250);
-            this.label1.TabIndex = 0;
-            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Счетчик:";
             // 
-            // button1
+            // button3
             // 
-            this.button1.Location = new System.Drawing.Point(604, 376);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button3.Location = new System.Drawing.Point(167, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 83);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Пассажир 2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(326, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 83);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Пассажир 3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(801, 450);
+            this.ClientSize = new System.Drawing.Size(496, 402);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Линейные программы №2 08.05/Линейные программы №2 08.05/Form1.cs
+++ Линейные программы №2 08.05/Линейные программы №2 08.05/Form1.cs
@@ -12,6 +12,10 @@ namespace Линейные_программы__2_08._05
 {
     public partial class Form1 : Form
     {
+        const int cost = 300;
+        const int costsittingdown = 30;
+        public bool flag = false;
+        public int k = 0;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +23,74 @@ namespace Линейные_программы__2_08._05
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Такси. В такси одновременно сели три пассажира. Когда вышел первый пассажир, на счетчике было р1 рублей; когда вышел второй — р2 рублей. Сколько должен был заплатить каждый пассажир, если по окончании поездки счетчик показал р3 рублей? Плата за посадку составляет р0 рублей. Тестирование: общая сумма оплаты пассажирами должна совпадать с показанием счетчика по окончании поездки. Рассмотрим крайние ситуации. По справедливости, если все три пассажира вышли одновременно, они должны заплатить по(р0 +р3)/ 3 руб.Если же первый и второй пассажиры «передумали ехать», они платят по р0 / 3 руб., а оставшаяся сумма ложится на счет третьего пассажира", "Задание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Такси. В такси одновременно сели три пассажира. Когда вышел первый пассажир, на счетчике было р1 рублей; когда вышел второй — р2 рублей. Сколько должен был заплатить каждый пассажир, если по окончании поездки счетчик показал р3 рублей? Плата за посадку составляет р0 рублей. Тестирование: общая сумма оплаты пассажирами должна совпадать с показанием счетчика по окончании поездки. Рассмотрим крайние ситуации. По справедливости, если все три пассажира вышли одновременно, они должны заплатить по(р0 +р3)/ 3 руб.Если же первый и второй пассажиры «передумали ехать», они платят по р0 / 3 руб., а оставшаяся сумма ложится на счет третьего пассажира\nНажатие на кнопки пассажиров имитирует их выход из такси (если до начала поездки - то считается как передумали ехать)", "Задание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            flag = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (k != 3)
+            {
+                try { progressBar1.Value++; }
+                catch
+                {
+                    timer1.Stop();
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    textBox1.Text = Convert.ToString(3 * cost);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Поездка завершена");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.BackgroundImage = new Bitmap(Image.FromFile("Resources\\k.png"));
+            b.BackgroundImageLayout = ImageLayout.Stretch;
+            switch(b.Text)
+            {
+                case "Пассажир 1":
+                    {
+                        if (flag == false)
+                        { textBox1.Text = Convert.ToString(Int32.Parse(textBox1.Text) + costsittingdown / 3);}
+                        else
+                        { textBox1.Text = Convert.ToString(Int32.Parse(textBox1.Text) + cost);}
+                        k++;
+                    }
+                    break;
+                case "Пассажир 2":
+                    {
+                        if (flag == false)
+                        { textBox1.Text = Convert.ToString(Int32.Parse(textBox1.Text) + costsittingdown / 3); }
+                        else
+                        { textBox1.Text = Convert.ToString(Int32.Parse(textBox1.Text) + cost); }
+                        k++;
+                    }
+                    break;
+                case "Пассажир 3":
+                    {
+                        if (flag == false)
+                        { textBox1.Text = Convert.ToString(Int32.Parse(textBox1.Text) + costsittingdown / 3); }
+                        else
+                        { textBox1.Text = Convert.ToString(Int32.Parse(textBox1.Text) + cost); }
+                        k++;
+                    }
+                    break;
+            }
+            b.Text = "";
         }
     }
 }
